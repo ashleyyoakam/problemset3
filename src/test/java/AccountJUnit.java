cf6efe153768fc021e4c9a4630794751721db7b8
ashleyyoakam <ashleyyoakam@wifi-roaming-128-4-191-206.host.udel.edu>
2016-10-04 14:01:02 -0400

    commit

---
+++ src/test/java/AccountJUnit.java
@@ -0,0 +1,80 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import Exceptions.InsufficientFundsException;
+
+public class AccountJUnit {
+	
+	Account Tester = new Account(1122,20000,4.5);
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		Tester.setBalance(20000);
+		Tester.setId(1122);
+		Tester.setAnnualInterestRate(4.5);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	
+	//test withdraw (true)
+	@Test
+	public void testWithdrawTrue() {
+		try {
+			assertTrue(Tester.withdraw(2500)==17500);
+		} catch (InsufficientFundsException e) {
+			e.printStackTrace();
+		}
+		System.out.println(Tester.getBalance());
+		System.out.println(Tester.getAnnualInterestRate());
+		System.out.println(Tester.getDateCreated());
+	}
+	
+	
+	//test withdraw (false)
+	@Test
+	public void testWithdrawFalse() {
+		try {
+			assertFalse(Tester.withdraw(2500)!=20000);
+		} catch (InsufficientFundsException e) {
+			e.printStackTrace();
+		}
+		System.out.println(Tester.getBalance());
+		System.out.println(Tester.getAnnualInterestRate());
+		System.out.println(Tester.getDateCreated());
+	}
+	
+	
+	@Test(expected=InsufficientFundsException.class)
+	public void testOverDraw() throws InsufficientFundsException{
+		Tester.withdraw(25000);
+	}
+	
+	
+	//test deposit (true)
+		@Test
+		public void testDepositTrue() {
+				assertFalse(Tester.deposit(3000)==23000);
+			
+			System.out.println(Tester.getBalance());
+			System.out.println(Tester.getAnnualInterestRate());
+			System.out.println(Tester.getDateCreated());
+		}
+		
+
+}
